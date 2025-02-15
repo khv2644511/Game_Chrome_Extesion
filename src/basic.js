b3f88ea70e4061485f02ea0a2ed7f76f26c4f280
khv2644511 <khv2644511@naver.com>
2025-02-16 02:15:22 +0900

    gui 제거

--- src/basic.js
+++ src/basic.js
@@ -391,19 +391,19 @@ export default function basic() {
   }
 
   // GUI
-  const gui = new GUI();
+  // const gui = new GUI();
 
-  const folderSky = gui.addFolder('Sky');
-  folderSky.add(parameters, 'elevation', 0, 90, 0.1).onChange(updateSun);
-  folderSky.add(parameters, 'azimuth', -180, 180, 0.1).onChange(updateSun);
-  folderSky.open();
+  // const folderSky = gui.addFolder('Sky');
+  // folderSky.add(parameters, 'elevation', 0, 90, 0.1).onChange(updateSun);
+  // folderSky.add(parameters, 'azimuth', -180, 180, 0.1).onChange(updateSun);
+  // folderSky.open();
 
-  const waterUniforms = water.material.uniforms;
+  // const waterUniforms = water.material.uniforms;
 
-  const folderWater = gui.addFolder('Water');
-  folderWater.add(waterUniforms.distortionScale, 'value', 0, 8, 0.1).name('distortionScale');
-  folderWater.add(waterUniforms.size, 'value', 0.1, 10, 0.1).name('size');
-  folderWater.open();
+  // const folderWater = gui.addFolder('Water');
+  // folderWater.add(waterUniforms.distortionScale, 'value', 0, 8, 0.1).name('distortionScale');
+  // folderWater.add(waterUniforms.size, 'value', 0.1, 10, 0.1).name('size');
+  // folderWater.open();
 
   // 그리기
   const clock = new THREE.Clock();
